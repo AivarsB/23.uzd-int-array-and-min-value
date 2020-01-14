a74248f675a2de56b83e10fc050cb1d9c7b7cbd9
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 15:43:44 +0200

    Add project files.

---
+++ 23.uzd int array and min value/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _23.uzd_int_array_and_min_value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 3, 4, 5, 6, 7, 1, 8, 9 };
+            var minValue = int.MaxValue;
+
+            foreach (var number in numbers)
+            {
+                if (number < minValue)
+                {
+                    minValue = number;
+                }
+            }
+
+            Console.WriteLine(minValue);
+        }
+    }
+}
